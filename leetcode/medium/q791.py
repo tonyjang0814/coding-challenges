79cd6dd6061988fe6e080ec922faa1daac38929e
Tony Jang <eui.chang@student.unsw.edu.au>
2021-01-03 20:31:09 +1100

    solved one problem

---
+++ leetcode/medium/q791.py
@@ -0,0 +1,9 @@
+class Solution:
+    #   method 1
+    def customSortString(self, S: str, T: str) -> str:
+        ans = ""
+        for i in S:
+            if i in T:
+                ans += (i * T.count(i)) # counts no. of times i occurs in T and adds that many i to ans
+                T = T.replace(i, '') # replaces all occurrences of i in T with '', so it doesn't repeat
+        return ans+T
